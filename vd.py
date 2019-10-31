29aa3b1120858b2146424914e83f6561dbf642a3
IulianStave <iulian.staverescu@gmail.com>
2019-10-31 11:04:40 +0200

    Save captured images

--- vd.py
+++ vd.py
@@ -9,11 +9,13 @@ while True:
 
     print(check)
     print(frame)
+    print(type(frame))
 
     if check:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow("Capturing webcam", gray)
-        key = cv2.waitKey(1)
+        cv2.imwrite("imgc"+str(countFrames)+".jpg",gray)
+        key = cv2.waitKey(1000)
         if key == ord('q'):
             break
     else:
